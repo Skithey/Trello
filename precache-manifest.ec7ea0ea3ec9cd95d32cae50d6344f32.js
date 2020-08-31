5ac63732f2c6fae60562e35dfcef6d454c5d4853
Nir <d.nir83@gmail.com>
2020-08-31 12:32:22 +0300

    Updates

---
+++ precache-manifest.ec7ea0ea3ec9cd95d32cae50d6344f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6519c360295cc29c52cb504fb5ada4ea",
+    "url": "/Trello%20/index.html"
+  },
+  {
+    "revision": "fcd5106beb910c29a31b",
+    "url": "/Trello%20/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c14694b9a1e8ebb1a866",
+    "url": "/Trello%20/static/js/2.8b5d2d1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Trello%20/static/js/2.8b5d2d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd5106beb910c29a31b",
+    "url": "/Trello%20/static/js/main.d3ab679a.chunk.js"
+  },
+  {
+    "revision": "7f7618b1fb22c03f094f",
+    "url": "/Trello%20/static/js/runtime-main.90af4672.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Trello%20/static/media/logo.5d5d9eef.svg"
+  }
+]);

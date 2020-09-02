c0abee1c99388907f45bc7fc000f627ac74d1e14
Itamar80 <65674792+Itamar80@users.noreply.github.com>
2020-09-02 16:57:24 +0300

    fixing bugs

--- src/cmps/BoardTopic/BoardTopic.jsx
+++ src/cmps/BoardTopic/BoardTopic.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 
 class BoardTopic extends Component {
     openModal = () =>{
-        console.log('haye');
+        console.log('hayeדד');
     }
     closeModal=()=>{
         console.log('modal is closed');
